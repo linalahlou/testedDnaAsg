e9456a55ffd3a09101602f720a9b806c5725e2fc
Lina Lahlou <linalahlou@dyn3162-178.wlan.ic.ac.uk>
2019-11-05 20:52:53 +0000

    feature(objectA): create class objectA

---
+++ a2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream> 
+#include <vector>
+
+class objectA { 
+    public: 
+        void load () { 
+
+        }
+        void save () { 
+
+        }
+        void analyse () { 
+
+        }
+        void search () { 
+
+        }
+       
+
+};
+
+int main () { 
+    
+    return 0;
+}
